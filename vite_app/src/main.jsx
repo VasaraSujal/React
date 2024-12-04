3ffe1bd976534f74cde9b67ad2a5754231acca00
Sujal Vasara <vasarasujal.cg@gmail.com>
2024-12-04 11:27:07 +0530

    Website is added 2

--- vite_app/src/main.jsx
+++ vite_app/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import HomePage from './HomePage.jsx'
-import './Homepage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
